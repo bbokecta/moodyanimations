53ad9937a06d808cdaec2c982af8f327a911350d
Bami <bami.oke@hogarth.com>
2024-12-05 12:56:52 +0000

    added three.js example model to flask site

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from chatbot import get_response 
 
-app = Flask(__name__) 
+app = Flask(__name__, static_url_path='/static') 
 
 new_question = "Hi!"
 
