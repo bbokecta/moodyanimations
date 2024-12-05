ab3dd0e0bc4ffc5c828d8811bdf86ee4b1c8300a
Bami <bami.oke@hogarth.com>
2024-12-05 12:17:31 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from chatbot import get_response 
+
+app = Flask(__name__) 
+
+new_question = "Hi!"
+
+@app.route('/')
+def index():
+    answer = get_response(new_question)
+    return render_template('index.html', question=new_question, bot_answer=answer, number=4)
+
+@app.route('/', methods=['POST'])
+def index_post():
+    # user_address1 = request.form['req_add1']
+    user_question = request.form['req_question']
+    answer = get_response(user_question)
+    bot_answer = answer.split('-')[0]
+    num_answer = answer.split('-')[1]
+    return render_template('index.html', question=user_question, bot_answer=bot_answer, number=num_answer)
---
+++ chatbot.py
@@ -0,0 +1,23 @@
+from openai import AzureOpenAI
+import os
+
+client = AzureOpenAI(
+	api_key = os.getenv("AZURE_KEY"),
+	azure_endpoint = os.getenv("AZURE_ENDPOINT"),
+	api_version = "2023-10-01-preview"
+	)
+
+
+
+def get_response(question):
+	my_messages = [
+	{"role": "system", "content": "Tell me your answer like a moody teenager, then after a hyphen, give me your answer in one word"},
+	{"role": "user", "content": question}
+]
+
+	response = client.chat.completions.create(
+		model = "GPT-4",
+		messages = my_messages
+		)
+
+	return response.choices[0].message.content
