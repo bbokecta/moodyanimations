4b437a2af222db9f468459dc47f80477bbdc73fa
Bami <bami.oke@hogarth.com>
2024-12-10 01:08:12 +0000

    changed api request method to use urllib instead of requests lib

--- telegrambot2.py
+++ telegrambot2.py
@@ -1,13 +1,17 @@
 import requests
+import urllib.request
+import json
 
 with open("telegram_keys.txt", "r") as telegram_file:
     telegram_key = telegram_file.read()
+url=f"https://api.telegram.org/bot{telegram_key}/getUpdates"
 
 def check_messages(): 
 
-    recent_message = requests.post(
-        url=f"https://api.telegram.org/bot{telegram_key}/getUpdates",
-        ).json()
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    
+    recent_message = json.loads(response.read())
 
     # chat_id = recent_message['result'][-1]['message']['chat']['id']
     message_contents = recent_message['result'][-1]['message']['text']
