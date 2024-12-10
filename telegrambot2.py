3b076ce612381739e7cb04c9ece368b549d65466
Bami <bami.oke@hogarth.com>
2024-12-10 01:03:44 +0000

    successfully getting incoming message by calling a function check_messages

--- telegrambot2.py
+++ telegrambot2.py
@@ -3,11 +3,15 @@ import requests
 with open("telegram_keys.txt", "r") as telegram_file:
     telegram_key = telegram_file.read()
 
-recent_message = requests.post(
-    url=f"https://api.telegram.org/bot{telegram_key}/getUpdates",
-    ).json()
+def check_messages(): 
 
-# chat_id = recent_message['result'][-1]['message']['chat']['id']
-message_contents = recent_message['result'][-1]['message']['text']
-# returned_data = [chat_id, message_contents]
-print(message_contents)
+    recent_message = requests.post(
+        url=f"https://api.telegram.org/bot{telegram_key}/getUpdates",
+        ).json()
+
+    # chat_id = recent_message['result'][-1]['message']['chat']['id']
+    message_contents = recent_message['result'][-1]['message']['text']
+    # returned_data = [chat_id, message_contents]
+    print(message_contents)
+
+check_messages()
